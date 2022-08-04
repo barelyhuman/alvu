d927bcd2debb8864184f2135fa655876e44ea479
Reaper <ahoy@barelyhuman.dev>
2022-08-05 01:09:29 +0530

    add readme cloner func

---
+++ docs/hooks/00-copy-readme.lua
@@ -0,0 +1,16 @@
+---@diagnostic disable-next-line: undefined-global
+local wdir = workingdir
+
+local json = require("json")
+
+function Writer(filedata)
+    local sourcedata = json.decode(filedata)
+    if sourcedata.name == "00-readme.html"
+    then 
+        local f = assert(io.open(wdir.."/../readme.md", "rb"))
+        local content = f:read("*all")
+        f:close()
+        sourcedata.content = content
+    end
+    return json.encode(sourcedata)
+end
